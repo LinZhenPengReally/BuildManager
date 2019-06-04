90fc422d44200f5c416425749af0008c27a46b6d
LinZhenPengReally <lzp13629752595@outlook.com>
2019-06-04 18:15:22 +0800

    springMVC JSON

--- src/main/java/controller/Hello.java
+++ src/main/java/controller/Hello.java
@@ -2,17 +2,23 @@ package controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class Hello {
@@ -30,6 +36,24 @@ public class Hello {
 	@RequestMapping("/to_multipart")
 	public String toMultipart(HttpServletRequest request, HttpServletResponse response) {
 		return "multipart";
+	}
+	
+	@RequestMapping("/message")
+	@ResponseBody
+	public String message(HttpSession session) {
+		Map<String,String> map=new HashMap<String, String>();
+        map.put("name", "lzp");
+        map.put("age", "26");
+        ObjectMapper mapper=new ObjectMapper();
+        String jsonString = null;
+		try {
+			jsonString = mapper.writeValueAsString(map);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println(jsonString);
+        return jsonString;
 	}
 	
 	@RequestMapping(path="/single", method=RequestMethod.POST)
